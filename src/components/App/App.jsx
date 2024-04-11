c06a8e9b3066e83de6783f1208e32d8f22c23eb9
TaichiKarna <taichikarna@gmail.com>
2024-04-11 22:38:26 +0530

    template-repo-react-tailwind-vitest

---
+++ src/components/App/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="border-teal-900 border-solid border-8">
+        <h1 className="text-3xl font-bold underline text-center border-solid border-blue-900 ">
+          Hello world!
+        </h1>
+    </div>
+  );
+}
